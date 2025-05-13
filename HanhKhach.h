cd5e7a4fab04e9be9af3338e2a836ee87f9b317a
anhnguyenpsk <anhnguyen12445@gmail.com>
2025-05-13 16:54:50 +0700

    Create header for HanhKhach

---
+++ HanhKhach.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+struct hanhKhach{
+    char CCCD[13];
+    char Ho[50];
+    char Ten[10];
+    char Phai[4];//Nam - Nu
+};
+
+typedef hanhKhach HK;
+
+struct nodeHK{
+    HK info;
+    nodeHK *left = NULL;
+    nodeHK *right = NULL;
+};
+
+typedef nodeHK *TREEHK;        //Binary search tree hanh khach
+
+
+//--------------------------------------------Cac module ve hanh khach----------------------------//
+void initTree(TREEHK &);
+int compareCCCD(HK , HK );
+void insertHK(TREEHK &, HK );
+void preorder(TREEHK );
+void clearErrTTHK(int , int );
+TREEHK searchHK(TREEHK , char [13]);
+int nhapHK(TREEHK &, char [13], int );
+void saveFileDSHK(char *, TREEHK );
+void loadFileDSHK(char *, TREEHK &);
